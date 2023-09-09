e8055bf7f1b5e56c6c2721ee61691f3749c740a1
Joy M. A. Salchert <lapsed_argent.0t@icloud.com>
2023-09-09 23:04:50 +0000

    in settings.py/paths, add: templates_dir. In settings.py/templates, set 'DIRS' to: templates_dir. In settings.py/hosts, set ALLOWED_HOSTS to: hash-love.herokuapp.com

--- hashlove/settings.py
+++ hashlove/settings.py
@@ -19,7 +19,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-joysalchert-pp4hashlove-e3w6lbbvhvo.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['hash-love.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'hashlove.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
