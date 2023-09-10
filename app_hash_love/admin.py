b690e8631f51ab413dde6b31e61d6d649b76a39d
Joy M. A. Salchert <lapsed_argent.0t@icloud.com>
2023-09-10 20:01:47 +0000

    in admin.py, add: django-summernote

--- app_hash_love/admin.py
+++ app_hash_love/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Placeholder_Model
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Placeholder_Model)
+class Placeholder_ModelAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
 
 # Register your models here.
