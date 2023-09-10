1cfb93b29fbbe86d2d811cf120c3f83448d57f66
Joy M. A. Salchert <lapsed_argent.0t@icloud.com>
2023-09-10 20:42:05 +0000

    in models.py & admin.py, add UserInfo model, did migration

--- app_hash_love/admin.py
+++ app_hash_love/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Placeholder_Model
+from .models import UserInfo
 from django_summernote.admin import SummernoteModelAdmin
 
-@admin.register(Placeholder_Model)
-class Placeholder_ModelAdmin(SummernoteModelAdmin):
-    summernote_fields = ('content')
+@admin.register(UserInfo)
+class UserInfoAdmin(SummernoteModelAdmin):
+    summernote_fields = '__all__'
 
 # Register your models here.
--- app_hash_love/models.py
+++ app_hash_love/models.py
@@ -3,3 +3,9 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=False, related_name='user_info', null=False)
+    bio = models.TextField('Bio', blank=True, null=True, default=None)
+    
